ba40f5ec1912a93c0ed752af719b6549330e228f
Arya Farel Abdillah <aryafarel.abd@gmail.com>
2022-06-15 22:03:24 +0700

    first commit

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright © <a href="https://instagram.com/ghifari0108_" target="_blank">Muhammad Nadhif Alghifari</a></span>
+		</div>
+	</div>
+</footer>
